3bd106c92cd6b226bb49144de74e28c7ab791a54
ByRon <calvobyron60@gmial.com>
2026-08-10 13:52:39 -0600

    Saying Facts

---
+++ src/Message/Daniel.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Message;
+
+/**
+ *
+ * @author Student
+ */
+public class Daniel {
+    private boolean vibeCoer;
+
+    public Daniel() {
+    }
+
+    public Daniel(boolean vibeCoer) {
+        this.vibeCoer = true;
+    }
+    
+    
+}
